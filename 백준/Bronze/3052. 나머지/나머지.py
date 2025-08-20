812d2f31d415b6161383eb2667f94a4035038898
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-08-20 15:34:45 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -3,9 +3,6 @@ import sys
 my_input = sys.stdin.readline
 
 N = 10
-answer = set()
+arr = { int(my_input()) % 42 for _ in range(N)}
 
-for _ in range(N):
-	answer.add(int(my_input()) % 42)
-
-print(len(answer))
+print(len(arr))
