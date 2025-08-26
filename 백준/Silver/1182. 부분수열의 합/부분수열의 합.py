9befca15f79987d43761392c4d71f28cb434d1f7
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-08-26 10:00:24 +0900

    [Silver II] Title: 부분수열의 합, Time: 368 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,22 +1,14 @@
 import sys
-
-def powerset(s):
-	masks = [1 << i for i in range(len(s))]
-
-	for i in range(1 << len(s)):
-		yield [ss for ss, mask in zip(s, masks) if mask & i]
+from itertools import combinations, chain
 
 my_input = sys.stdin.readline
 
 N, S = map(int, my_input().split())
 arr = list(map(int, my_input().split()))
-
 answer = 0
-for power in powerset(arr):
-	if len(power) == 0:
-		continue
 
-	if sum(power) == S:
+for i in chain.from_iterable(combinations(arr, r) for r in range(1, N+1)):
+	if sum(i) == S:
 		answer += 1
 
 print(answer)
