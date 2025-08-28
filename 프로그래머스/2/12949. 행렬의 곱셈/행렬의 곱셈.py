5c0038aaf6b725f2791335dfc2e1704846946643
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-08-28 11:47:26 +0900

    [level 2] Title: 행렬의 곱셈, Time: 41.44 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,13 +1,3 @@
 def solution(arr1, arr2):
-	ax = len(arr1)
-	bx = len(arr2)
-	by = len(arr2[0])
-
-	answer = [[0 for _ in range(by)] for _ in range(ax)]
-
-	for i in range(ax):
-		for j in range(by):
-			for k in range(bx):
-				answer[i][j] += arr1[i][k] * arr2[k][j]
-
-	return answer
+    arr2_zipped = list(zip(*arr2))
+    return [[sum( a*b for a, b in zip(row, row2)) for row2 in arr2_zipped] for row in arr1]
