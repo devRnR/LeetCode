e4d902a7711b7b358642154ccb2b0875592e30e7
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-08-28 09:35:44 +0900

    [level 1] Title: 모의고사, Time: 5.28 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -6,15 +6,15 @@ def solution(answers):
     ]
     
     scores = [0, 0, 0]
-    for i in range(len(answers)):
+    for i, a in enumerate(answers):
         
-        scores[0] += 1 if patterns[0][i % len(patterns[0])] == answers[i] else 0
-        scores[1] += 1 if patterns[1][i % len(patterns[1])] == answers[i] else 0
-        scores[2] += 1 if patterns[2][i % len(patterns[2])] == answers[i] else 0
+        scores[0] += 1 if patterns[0][i % len(patterns[0])] == a else 0
+        scores[1] += 1 if patterns[1][i % len(patterns[1])] == a else 0
+        scores[2] += 1 if patterns[2][i % len(patterns[2])] == a else 0
         
     answer = []
-    for i in range(len(scores)):
-        if scores[i] == max(scores):
+    for i, score in enumerate(scores):
+        if score == max(scores):
             answer.append(i + 1)
     
     
