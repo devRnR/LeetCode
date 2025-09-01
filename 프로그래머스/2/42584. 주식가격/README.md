9f456a2f41667184a2efb8a77469e773ff779b2d
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-09-01 12:51:20 +0900

    [level 2] Title: 주식가격, Time: 29.16 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 18.6 MB, 시간: 68.58 ms
+메모리: 19 MB, 시간: 29.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 30일 22:55:19
+2025년 09월 01일 12:51:17
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,36 @@
-from collections import deque
-def solution(prices):
-    result = []
-    queue = deque(prices)
+# from collections import deque
+# def solution(prices):
+#     result = []
+#     queue = deque(prices)
     
-    while queue:
-        period = 0
-        price = queue.popleft()
+#     while queue:
+#         period = 0
+#         price = queue.popleft()
         
-        for after in queue:
-            period += 1
-            if price > after:
-                break
+#         for after in queue:
+#             period += 1
+#             if price > after:
+#                 break
                 
-        result.append(period)
+#         result.append(period)
         
-    return result
+#     return result
+
+def solution(prices):
+    n = len(prices)
+    stack = [0]
+    answer = [0]  * n
+    
+    for i in range(1, n):
+        while stack and prices[i] < prices[stack[-1]]:
+            j = stack.pop()
+            answer[j] = i - j
+        
+        stack.append(i)
+    
+    while stack:
+        j = stack.pop()
+        answer[j] = n - 1 - j
+        
+    return answer
+    
