2204ad1c4d742cafd4ad6e549675dd29fb4d232a
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-09-04 14:47:26 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 62.13 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.36 ms
+메모리: 9.3 MB, 시간: 62.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 22:38:31
+2025년 09월 04일 14:47:23
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,24 @@
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    
+    trucks = deque(truck_weights)
+    bridge = deque([])
+    
+    time = 1
+    nw = 0
+    while trucks or bridge:
+        while bridge:
+            if time - bridge_length == bridge[0][1]:
+                pt = bridge.popleft()
+                nw -= pt[0]
+            else:
+                break
+
+        if trucks and nw + trucks[0] <= weight and len(bridge) < bridge_length:
+            t = trucks.popleft()
+            bridge.append((t, time))
+            nw += t
+            
+        time += 1
+    return time - 1
