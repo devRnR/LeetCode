96d2eada414f142d93a9336f35915044037ad9f4
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-09-04 12:50:01 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,27 +1,20 @@
-from collections import deque
 from math import ceil
 
-def solution(progresses, speeds):
+def solution(progoresses, speeds):
+    days = [ceil((100-p)/s) for p, s in zip(progoresses, speeds)]
     answer = []
-    pq = deque([ list(ps) for ps in zip(progresses, speeds)])
-    goal = 100
-    while pq:
-        remain = goal - pq[0][0]
-        remain_days =  ceil((100 - pq[0][0]) / pq[0][1])
-        
-        for e in pq:
-            e[0] += remain_days * e[1]
-        
-        cnt = 0
-        while pq:
-            e = pq.popleft()
-            
-            if e[0] < goal:
-                pq.appendleft(e)
-                break
-            
+    
+    cur = days[0]
+    cnt = 0
+    
+    for d in days:
+        if d <= cur:
             cnt += 1
-        
-        answer.append(cnt)
-        
+        else:
+            answer.append(cnt)
+            cur = d
+            cnt = 1
+    
+    answer.append(cnt)
+
     return answer
