d0ee51d4db83c973dbb97b55b804e8a4b633990d
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-09-05 16:11:58 +0900

    [level 2] Title: 오픈채팅방, Time: 127.16 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,24 +1,18 @@
 def solution(record):
     answer = []
-    
     users = {}
+    prints = {'Enter': '님이 들어왔습니다.', 'Leave': '님이 나갔습니다.'}
     
     for r in record:
-        h = list(r.split())
+        h = r.split()
         
-        command = h[0]
-        user_id = h[1]
+        if h[0] == 'Enter' or h[0] == 'Change':
+            users[h[1]] = h[2]
+    
+    for r in record:
+        h = r.split()
         
-        if command == 'Enter':
-            answer.append(["*님이 들어왔습니다.", user_id])
-            users[user_id] = h[2]
-        elif command == 'Leave':
-            answer.append(["*님이 나갔습니다.", user_id])
-        else:
-            users[user_id] = h[2]
+        if h[0] != 'Change':
+            answer.append(f'{users[h[1]]}{prints[h[0]]}')
         
-    for a in answer:
-        nick = users[a[1]]
-        a[0] = a[0].replace("*", nick)        
-        
-    return [a[0] for a in answer]
+    return answer
