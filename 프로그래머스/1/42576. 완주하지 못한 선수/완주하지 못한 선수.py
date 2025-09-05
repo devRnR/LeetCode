0b99ce1696973876dd0747bc36d1afcdf02998e1
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-09-05 16:15:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.15 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,3 @@
+from collections import Counter
 def solution(participant, completion):
-    answer = ''
-    pd = {}
-    
-    for p in participant:
-        pd[p] = pd.get(p, 0) + 1
-    
-    for c in completion:
-        pd[c] -= 1
-        
-    for k, v in pd.items():
-        if v > 0:
-            answer = k
-            break
-
-    return answer
+    return list(Counter(participant) - Counter(completion))[0]
