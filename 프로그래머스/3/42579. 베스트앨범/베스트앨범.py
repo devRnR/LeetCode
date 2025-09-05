4c62fc8f45fb0ea9da5f1d4a2e760f155ae884cb
DevR&R <82052272+devRnR@users.noreply.github.com>
2025-09-05 16:44:54 +0900

    [level 3] Title: 베스트앨범, Time: 0.16 ms, Memory: 9.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,18 @@
+def solution(genres, plays):
+    answer = []
+    
+    gp = {}
+    pd = {}
+    for g, p in zip(genres, plays):
+        pd[g] = []
+        gp[g] = gp.get(g, 0) + p
+        
+    for i, e in enumerate(zip(genres, plays)):
+        pd[e[0]].append([i, e[1]])
+        
+    for i, v in pd.items():
+        pd[i] = list(map(lambda x: x[0], sorted(v, key = lambda x: (-x[1], x[0]))))
+    for genre in list(map(lambda x: x[0], sorted(gp.items(), key = lambda x: -x[1]))):
+        answer += pd[genre][:2]
+    
+    return answer
