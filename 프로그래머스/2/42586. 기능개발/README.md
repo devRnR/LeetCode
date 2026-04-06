0f17f3a522593cf3faf11f3a3ac9fb5957064998
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-06 11:08:26 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 0.04 ms
+메모리: 9.07 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 04일 12:49:58
+2026년 04월 06일 11:08:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,21 @@
 from math import ceil
+from collections import deque
 
 def solution(progoresses, speeds):
-    days = [ceil((100-p)/s) for p, s in zip(progoresses, speeds)]
-    answer = []
     
-    cur = days[0]
-    cnt = 0
-    
-    for d in days:
-        if d <= cur:
-            cnt += 1
-        else:
-            answer.append(cnt)
-            cur = d
-            cnt = 1
-    
-    answer.append(cnt)
+    q = deque(zip(progoresses, speeds))
+    deploy = []
 
-    return answer
+    while q:
+        cnt = 1
+        
+        p, s = q.popleft()
+        m = ceil((100 - p ) / s)
+        
+        while q and (q[0][1] * m + q[0][0]) >= 100:
+            cnt += 1
+            q.popleft()
+
+        deploy.append(cnt)
+
+    return deploy
