4a89a4793ec591422fa47a20d784371878f73c7e
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-07 11:45:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.82 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 38.1 MB, 시간: 74.15 ms
+메모리: 33 MB, 시간: 46.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 05일 16:15:19
+2026년 04월 07일 11:45:14
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,13 @@
-from collections import Counter
 def solution(participant, completion):
-    return list(Counter(participant) - Counter(completion))[0]
+    pd = {}
+    
+    for p in participant:
+        pd[p] = pd.setdefault(p, 0) + 1
+        
+    for c in completion:
+        if c in pd:
+            pd[c] -= 1
+        
+    for k, v in pd.items():
+        if v > 0:
+            return k
