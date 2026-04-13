2f5222de6225d39f46f4bacb6df2d0f18c3727ee
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-13 12:52:10 +0900

    [level 1] Title: 신고 결과 받기, Time: 1505.93 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,18 +1,12 @@
 def solution(id_list, report, k):
-    ban = {}
-    id = {k: i for i, k in enumerate(id_list)}
+    ban = {r.split()[1]: 0 for r in report}
     answer = [0 for _ in range(len(id_list))]
     
-    for r in report:
-        f, t = r.split()
-        if t in ban:
-            ban[t].add(f)
-        else:
-            ban[t] = {f,}
-
-    for _, v in ban.items():
-        if len(v) >= k:
-            for w in v:
-                answer[id[w]] += 1
+    for r in set(report):
+        ban[r.split()[1]] += 1
+        
+    for r in set(report):
+        if ban[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
     
     return answer
