6cc81c594f4804bd7c4d5a6b37bfd05242153e79
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-14 15:38:10 +0900

    [Silver IV] Title: 수 찾기, Time: 464 ms, Memory: 49136 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,10 +2,27 @@ import sys
 
 input = sys.stdin.readline
 n = int(input())
-n_arr = { k: 1 for k in map(int, input().split()) }
+n_arr = sorted(map(int, input().split()))
 
 m = int(input())
 m_arr = list(map(int, input().split()))
 
+def bs(arr, target):
+
+    l, r = 0, len(arr) - 1
+
+    while l <= r:
+        mid = l + (r - l) // 2
+
+        if arr[mid] == target:
+            return 1
+        elif target < arr[mid]:
+            r = mid - 1
+        else:
+            l = mid + 1
+    return 0
+
 for num in m_arr:
-    print(1 if num in n_arr else 0)
+    print(bs(n_arr, num))
+
+
