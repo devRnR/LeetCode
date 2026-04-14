086b3bca56d8e6a2fc60da1389a5398ab70f6714
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-14 15:39:46 +0900

    [Silver IV] Title: 수 찾기, Time: 224 ms, Memory: 51188 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,28 +1,20 @@
 import sys
+from bisect import *
 
 input = sys.stdin.readline
+
 n = int(input())
 n_arr = sorted(map(int, input().split()))
 
 m = int(input())
 m_arr = list(map(int, input().split()))
 
-def bs(arr, target):
-
-    l, r = 0, len(arr) - 1
-
-    while l <= r:
-        mid = l + (r - l) // 2
-
-        if arr[mid] == target:
-            return 1
-        elif target < arr[mid]:
-            r = mid - 1
-        else:
-            l = mid + 1
-    return 0
-
 for num in m_arr:
-    print(bs(n_arr, num))
+    v = bisect_left(n_arr, num)
+
+    if v < len(n_arr) and n_arr[v] == num:
+        print(1)
+    else:
+        print(0)
 
 
