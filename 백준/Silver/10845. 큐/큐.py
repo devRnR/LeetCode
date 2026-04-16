63e4d5d8e74c03acf1174471cc9f2480e51c9d80
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-16 14:11:21 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -6,17 +6,64 @@ n = int(input())
 
 q = deque()
 
+class CircularQueue:
+	def __init__(self, k):
+
+		self.q = [None] * k
+		self.front_idx = 0
+		self.rear_idx = 0
+		self.size = 0
+		self.max_size = k
+
+
+	def enqueue(self, value):
+		if self.size == self.max_size:
+			raise IndexError("Queue is full")
+
+		self.q[self.rear_idx] = value
+		self.rear_idx = (self.rear_idx + 1) % self.max_size
+		self.size += 1
+
+	def dequeue(self):
+		if self.size == 0:
+			return -1
+
+		value = self.q[self.front_idx]
+		self.front_idx = (self.front_idx + 1) % self.max_size
+		self.size -= 1
+		return value
+
+	def front(self):
+		if self.size == 0:
+			return -1
+		return self.q[self.front_idx]
+
+	def back(self):
+		if self.size == 0:
+			return -1
+
+		last_index = (self.rear_idx - 1 + self.max_size) % self.max_size
+		return self.q[last_index]
+
+	def get_size(self):
+		return self.size
+
+
+q = CircularQueue(100000)
 for _ in range(n):
+
 	command = list(input().split())
 	if command[0] == 'push':
-		q.append(command[1])
+		q.enqueue(command[1])
 	elif command[0] == 'pop':
-		print(q.popleft() if q else -1)
+		print(q.dequeue())
 	elif command[0] == 'size':
-		print(len(q))
+		print(q.get_size())
 	elif command[0] == 'empty':
-		print(0 if q else 1)
+		print(1 if q.get_size() == 0 else 0)
 	elif command[0] == 'front':
-		print(q[0] if q else -1)
+		print(q.front())
 	elif command[0] == 'back':
-		print(q[-1] if q else -1)
+		print(q.back())
+
+
