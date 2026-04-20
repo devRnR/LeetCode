d8416a9e1d415295990fac984f9aeba33c8b1e64
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-20 15:28:24 +0900

    Time: 4924 ms (5.97%), Space: 25 MB (22.17%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -5,11 +5,10 @@ class RecentCounter:
     
     def ping(self, t: int) -> int:
         min = t - 3000
-        max = t
         count = 1
 
         for q in self.queue:
-            if max >= q >= min:
+            if q >= min:
                 count += 1
         
         self.queue.append(t)
