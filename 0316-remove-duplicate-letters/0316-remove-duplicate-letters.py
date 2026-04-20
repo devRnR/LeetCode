d35b6acdeddd1dc32af93a9cd41784237c130ced
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-20 15:00:10 +0900

    Time: 1 ms (77.96%), Space: 19.2 MB (84.43%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -2,18 +2,13 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         check = set()
         stack = []
-        memo = {}
-        for c in s:
-            memo[c] = memo.get(c, 0) + 1
+        last = {c:i for i, c in enumerate(s)}
 
-        for c in s:
-
-            memo[c] -= 1 
-
+        for i, c in enumerate(s):
             if c in check:
                 continue
 
-            while stack and stack[-1] > c and memo[stack[-1]]> 0:
+            while stack and stack[-1] > c and last[stack[-1]]> i:
                 v = stack.pop()
                 check.remove(v)
 
