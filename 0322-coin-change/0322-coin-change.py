376aae4fec1afc1bf3d69c98a8da2044d756252f
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-22 15:03:07 +0900

    Time: 503 ms (70.83%), Space: 19.5 MB (79.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -24,14 +24,12 @@ class Solution:
         
         def dp_up(n):
 
-            dp = [ amount + 1 for _ in range(amount + 1) ]
+            dp = [ amount + 1 ] * (amount + 1)
             dp[0] = 0
 
             for i in range(1, amount+1):
                 for c in coins:
-
                     if i - c < 0: continue
-
                     dp[i] = min(dp[i], dp[i-c] + 1)
             
             return dp[amount] if dp[amount] != amount + 1 else -1
