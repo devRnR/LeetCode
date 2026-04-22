45828240896d74fa1c6f522fe511c9b0bb7c6485
DevR&R <82052272+devRnR@users.noreply.github.com>
2026-04-22 16:13:49 +0900

    Time: 0 ms (100%), Space: 19.4 MB (22.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -12,5 +12,14 @@ class Solution:
             memo[n] = dp(n-1) + dp(n-2) 
             return memo[n]
         
-        return dp(n)
+        def dp_2(n):
+            memo = [0] * (n+1)
+            memo[0], memo[1] = 1, 1
+
+            for i in range(2, n+1):
+                memo[i] = memo[i-1] + memo[i-2]
+
+            return memo[n]
+
+        return dp_2(n)
         
